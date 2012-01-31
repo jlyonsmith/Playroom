c62563eb1142e8c42de4d2b36ce486b79062efa0
John Lyon-Smith <john@lyon-smith.org>
2012-01-31 00:06:39 -0800

    Adding VS ItemTemplates.

--- Scripts/DeployToDropBox.bat
+++ Scripts/DeployToDropBox.bat
@@ -4,5 +4,5 @@ set Config=%1
 if "%Config%"=="" set Config=Release
 set TargetDir=%HomePath%\Documents\My Dropbox\Jamoki\Software\Playroom
 md "%TargetDir%"
-call XCopyFiles.bat
+call %SolutionRoot%\Scripts\XCopyFiles.bat
 endlocal
--- Scripts/DeployToMSBuild.bat
+++ Scripts/DeployToMSBuild.bat
@@ -5,8 +5,8 @@ set Config=%1
 if "%Config%"=="" set Config=Release
 set TargetDir=c:\program files (x86)\MSBuild\Playroom
 md "%TargetDir%"
-call XCopyFiles.bat
+call %SolutionRoot%\Scripts\XCopyFiles.bat
 set TargetDir=c:\program files\MSBuild\Playroom
 md "%TargetDir%"
-call XCopyFiles.bat
+call %SolutionRoot%\Scripts\XCopyFiles.bat
 endlocal
