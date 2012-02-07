0c62631816de4c58b132ecae83277c936cd6d1ba
John Lyon-Smith <john@lyon-smith.org>
2012-02-06 18:32:15 -0800

    Refactor MouseArrowComponent int MousePointerManager/IMousePointerService.

--- ToyBox/MousePointerManager.cs
+++ ToyBox/MousePointerManager.cs
@@ -4,21 +4,44 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ToyBox
 {
 #if WINDOWS
 
-    public class MouseArrowComponent : GameComponent
+    public class MousePointerManager : GameComponent, IMousePointerService
     {
         private ISpriteService spriteService;
         private IInputService inputService;
         private Texture2D mousePointerTexture;
         private TextureSprite mouseSprite;
+        private Point mousePosition;
 
-        public MouseArrowComponent(Game game)
+        public MousePointerManager(Game game)
             : base(game)
         {
+            if (this.Game.Services != null)
+            {
+                this.Game.Services.AddService(typeof(IMousePointerService), this);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+#if WINDOWS
+                this.spriteService.DetachSprite(mouseSprite);
+#endif
+
+                if (this.Game.Services != null)
+                {
+                    this.Game.Services.RemoveService(typeof(IMousePointerService));
+                }
+            }
+
+            base.Dispose(disposing);
         }
 
         public override void Initialize()
@@ -28,6 +51,7 @@ namespace ToyBox
             inputService = (IInputService)this.Game.Services.GetService(typeof(IInputService));
             spriteService = (ISpriteService)this.Game.Services.GetService(typeof(ISpriteService));
 
+#if WINDOWS
             this.mousePointerTexture = this.Game.Content.Load<Texture2D>("Textures/Arrow");
             this.mouseSprite = new TextureSprite(
                     new SpriteTexture(this.mousePointerTexture, null),
@@ -35,26 +59,30 @@ namespace ToyBox
                     0, true, null);
             
             spriteService.AttachSprite(this.mouseSprite);
+#endif
 
             IMouse mouse = this.inputService.GetMouse();
+            MouseState mouseState = mouse.GetState();
+
+            mousePosition = new Point (mouseState.X, mouseState.Y);
 
             mouse.Moved += new MouseMovedDelegate(Mouse_Moved);
         }
 
         private void Mouse_Moved(Point point)
         {
-            this.mouseSprite.Position = point;
+            mousePosition = point;
+            this.mouseSprite.Position = mousePosition;
         }
 
-        protected override void Dispose(bool disposing)
+        #region IMouseArrowService Members
+
+        public Point Position
         {
-            if (disposing)
-            {
-                this.spriteService.DetachSprite(mouseSprite);
-            }
-
-            base.Dispose(disposing);
+            get { return mousePosition; }
         }
+
+        #endregion
     }
 
 #endif
